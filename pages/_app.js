18f8a4e4ec6dc231166868931d8c51d96c94aacc
achuthhadnoor <armorachyuth123@gmail.com>
2020-12-29 20:53:24 +0530

    theme added

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,15 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { ThemedButton, ThemeWrapper } from '../components/Theme'
 import AppComponent from './../components/App'
-import Image from 'next/image'
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    font-family:sans-serif;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    background: #121212;
-    color:#fff;
-    background:url('/light.png')
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <ThemeProvider theme={theme}>
-      <GlobalStyle />
+      <ThemeWrapper> 
         <AppComponent/>
+        <ThemedButton/>
         <Component {...pageProps} />
-      </ThemeProvider>
+      </ThemeWrapper>
     </>
   )
 }
