9238aaf08259dd7bb5e9885f04f9efaa4d3d3582
Achuth Hadnoor <armorachyuth123@gmail.com>
2021-05-13 20:47:11 +0530

    changes for gumroad button

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ function Home({ posts }) {
           <p className="p-4 text-lg">Are you tired of using multiple softwares to record long hour screen recording and then converting it to time-lapse?</p>
           <p className="p-4 text-lg">Lapse will help you to instantly record in timelapse in three simple clicks</p>
           <div className="p-2 flex items-center space-x-4">
-            <button className="p-2 bg-white rounded-md">Get it now</button>
+          <a class="gumroad-button" href="https://gum.co/lapse_app"><>Get it now</></a>
             <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M12.861 4.55483C13.3623 3.95654 13.71 3.13997 13.71 2.31531C13.71 2.20212 13.7019 2.08893 13.6776 2C12.8772 2.03234 11.907 2.5336 11.3249 3.21273C10.8722 3.73017 10.4517 4.55483 10.4517 5.37948C10.4517 5.50884 10.4679 5.63012 10.4841 5.67054C10.5326 5.67863 10.6134 5.6948 10.6943 5.6948C11.4219 5.6948 12.3274 5.2097 12.861 4.55483ZM13.4351 5.86458C12.2223 5.86458 11.2441 6.6003 10.6134 6.6003C9.9424 6.6003 9.06923 5.91309 8.01819 5.91309C6.02122 5.91309 4 7.56241 4 10.667C4 12.6074 4.74381 14.6529 5.67357 15.9707C6.46589 17.0864 7.16119 18 8.15563 18C9.15008 18 9.58666 17.3451 10.8075 17.3451C12.0606 17.3451 12.3355 17.9838 13.4351 17.9838C14.5184 17.9838 15.238 16.9894 15.9171 16.0111C16.6852 14.8873 17.0005 13.7959 17.0167 13.7393C16.952 13.7231 14.8742 12.8742 14.8742 10.5053C14.8742 8.45174 16.5073 7.53007 16.5963 7.4573C15.5291 5.91309 13.8878 5.86458 13.4351 5.86458Z" fill="black" fill-opacity="0.9" />
             </svg>
@@ -78,7 +78,7 @@ function Home({ posts }) {
       </section> <div className="p-10 rounded-b-full bg-green-200 "></div>
       <section className="mx-auto max-w-screen-lg" id="features">
         <div className=" flex flex-col md:flex-row text-gray-400 p-4  items-center">
-          <video className="" width="480" height="320" src="videos/lapse.webm" autoplay controls />
+          <video className="" width="480" height="320" src="videos/lapse.webm" autoPlay controls />
           <div className="px-4">
             <h3 className="text-2xl font-semibold py-4 text-green-300">Timelapse recording on fingertips..</h3>
             <p>We almost made zero UI to make it simple and easy to use. This helps you record more and learn less.</p>
@@ -87,7 +87,7 @@ function Home({ posts }) {
         <h2 className="p-4 text-green-200 text-4xl my-10">Features</h2>
         <div className="p-4 text-white grid gap-10 lg:grid-cols-3 md:grid-cols-2 ">
           <div className="grid ">
-            <video width="320" height="240" src="/videos/vid-1.webm" type="video/webm" autoplay controls />
+            <video width="320" height="240" src="/videos/vid-1.webm" type="video/webm" autoPlay controls />
             <div>
               <h3 className="text-2xl font-semibold py-4 text-green-300">
                 Export options
@@ -98,7 +98,7 @@ function Home({ posts }) {
             </div>
           </div>
           <div>
-            <video width="320" height="240" src="/videos/vid-2.webm" type="video/webm" autoplay controls />
+            <video width="320" height="240" src="/videos/vid-2.webm" type="video/webm" autoPlay controls />
             <div>
               <h3 className="text-2xl font-semibold py-4 text-green-300">
                 Video Quality
@@ -109,7 +109,7 @@ function Home({ posts }) {
             </div>
           </div>
           <div>
-            <video width="320" height="240" autoplay controls src="/videos/vid-3.webm" type="video/webm" />
+            <video width="320" height="240" autoPlay controls src="/videos/vid-3.webm" type="video/webm" />
             <div>
               <h3 className="text-2xl font-semibold py-4 text-green-300">
                 Speed (Framerate)
@@ -166,6 +166,9 @@ function Home({ posts }) {
             </div>
           </div>
         </div>
+      </section>
+      <section className="mx-auto max-w-screen-lg text-gray-400">
+        <h2 className="p-4 text-green-200 text-4xl my-10">Feedback</h2>
       </section>
     </Container >)
 }
