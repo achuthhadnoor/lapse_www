c230900e24d7d039beecafb937905992c3f7f081
achuthhadnoor <armorachyuth123@gmail.com>
2021-09-01 23:56:40 +0530

    verify changes

--- pages/appsumo.js
+++ pages/appsumo.js
@@ -28,21 +28,22 @@ const AppSumo = (props) => {
     }
     return (
         <Container>
+          {  !loading ? <div className="relative h-full w-md">
             <h1 className="text-4xl text-gray-200 text-center mt-10">Lapse 💚 <img className="w-200 inline-block" src="https://brandox-production.s3-eu-central-1.amazonaws.com/6e2aa086-61bb-42db-9d4e-1bb8f97c20e0/as-appsumo-logo--1200x1200.png" width="150px" /></h1>
-            <p className="text-md leading-loose text-gray-200 max-w-4xl mx-4 md:mx-auto mt-10">Hey Sumo-lings 👋,<br/>
+            <p className="text-md leading-loose text-gray-200 max-w-4xl mx-4 xl:mx-auto mt-10">Hey Sumo-lings 👋,<br/>
                 Please enter your name, email and Appsumo code below to download lapse and 
                 enjoy creating amaizing time-lapse videos  
                 <span className="mx-1 p-1 bg-gray-800 rounded"> with life time access</span>.
             </p> 
             <br/>
-            <p className="text-gray-200 max-w-4xl mx-4 md:mx-auto my-2">
+            <p className="text-gray-200 max-w-4xl mx-4 xl:mx-auto my-2">
                 After submission, you will recieve the  <span className="mx-1 p-1 bg-gray-800 rounded"> dmg file </span> where you will have to enter both email and the code 
             </p>
-            <p className="text-gray-200 max-w-4xl mx-4 md:mx-auto my-5">
+            <p className="text-gray-200 max-w-4xl mx-4 xl:mx-auto my-5">
                 Thank you,<br/>
                 Achuth Hadnoor
             </p>
-            <form onSubmit={submitApi} className=" mb-20 flex flex-col max-w-4xl bg-gray-200 p-5 mx-5 md:mx-auto rounded">
+            <form onSubmit={submitApi} className=" mb-20 flex flex-col max-w-4xl bg-gray-200 p-5 mx-5 xl:mx-auto rounded">
                 <h2 className="text-2xl">Start recoding your amaizing digital work in time-lapse</h2>
                 {/* <span className="text-red-600 text-xl">* required</span><br/> */}
                 <input 
@@ -70,6 +71,14 @@ const AppSumo = (props) => {
                     </button>
                     </div>
             </form>
+        
+        </div> : <div className="h-screen">
+                <h1 className="text-4xl text-gray-200 text-center mt-10">Lapse 💚 <img className="w-200 inline-block" src="https://brandox-production.s3-eu-central-1.amazonaws.com/6e2aa086-61bb-42db-9d4e-1bb8f97c20e0/as-appsumo-logo--1200x1200.png" width="150px" /></h1>
+            <p className="text-gray-200 max-w-4xl mx-4 xl:mx-auto my-2 h-full relative">
+                🦄 Verifying your code... 
+                </p>
+            </div>
+            }
         </Container>
     )
 };
