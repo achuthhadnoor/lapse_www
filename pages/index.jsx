e6fc3ff5e3c46320a1da65e130ea927595896aab
achuthhadnoor <armorachyuth123@gmail.com>
2022-02-03 21:28:08 +0530

    download

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,143 @@
 import Container from '../components/Container'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
+import {useState,useEffect} from 'react'
+
+const TimerComp = ({ type }) => {
+  const [timer, setTimer] = useState({ days: 0, hr: 0, min: 0, sec: 0 });
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+
+    const countDownDate = new Date("Mar 5, 2022 15:37:25").getTime();
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      setTimer({ days: days, hr: hours, min: minutes, sec: seconds })
+    setIsLoading(false)
+
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+      }
+    }, 1000);
+  }, []);
+
+  switch (type) {
+    case 'vertical':
+      return (
+        <div id="timer" className='mt-5'>
+          {!isLoading && <div className='inline-block rounded-xl bg-skin-secondary align-middle justify-center p-2'>
+            <div className='flex'>
+              <span className='flex flex-col p-2 mr-4'>
+                <span>{timer.days}</span>
+                <span className='text-xs text-indigo-500'>Days</span>
+              </span>
+              <span className='flex flex-col p-2 mr-4'>
+                <span>{timer.hr}</span>
+                <span className='text-xs text-indigo-500'>Hrs</span>
+              </span>
+              <span className='flex flex-col p-2 mr-4'>
+                <span>{timer.min}</span>
+                <span className='text-xs text-indigo-500'>mins</span>
+              </span>
+              <span className='flex flex-col p-2 mr-4'>
+                <span>{timer.sec}</span>
+                <span className='text-xs text-indigo-500'>Sec</span>
+              </span>
+            </div>
+          <a href="https://achuthhadnoor.gumroad.com/l/learnvim">
+            <button className='px-4 py-2 mt-2 bg-orange-500 w-full rounded-lg hover:ring-2 ring-purple-900 text-xs sm:text-sm'>
+              Get It for <span className='line-through stroke-orange-600 text-purple-300'>$35</span> <span className='text-gray-900'>$25</span>
+            </button>
+            </a>
+          </div>}
+        </div>
+      )
+    case 'horizontal':
+      return (
+        <div id="timer" className='mt-4'>
+          {!isLoading && <div className='inline-block rounded-xl bg-skin-secondary align-middle justify-center'>
+            {/* <div className='flex'>
+              <span className='flex flex-col p-2 mr-4'>
+                <span>{timer.days}</span>
+                <span className='text-xs text-orange-500'>Days</span>
+              </span>
+              <span className='flex flex-col p-2 mr-4'>
+                <span>{timer.hr}</span>
+                <span className='text-xs text-orange-500'>Hrs</span>
+              </span>
+              <span className='flex flex-col p-2 mr-4'>
+                <span>{timer.min}</span>
+                <span className='text-xs text-orange-500'>mins</span>
+              </span>
+              <span className='flex flex-col p-2 mr-4'>
+                <span>{timer.sec}</span>
+                <span className='text-xs text-orange-500'>Sec</span>
+              </span>
+          <a href="https://achuthhadnoor.gumroad.com/l/learnvim">
+              <button className='px-4 py-2 bg-orange-500 m-2 rounded-lg hover:ring-2 ring-purple-900 text-xs sm:text-sm'>
+                Get It for <span className='line-through stroke-orange-600 text-purple-300'>$35</span> <span className='text-gray-900'>$25</span>
+              </button>
+            </a>
+            </div> */}
+             <div className="mt-5 inline-block rounded-xl bg-skin-secondary align-middle justify-center bg-orange-200 px-2">
+              <div className="flex">
+                <span className="flex flex-col p-2 mr-4">
+                  <span>{timer.days}</span>
+                  <span className="text-xs text-orange-500">Days</span>
+                </span>
+                <span className="flex flex-col p-2 mr-4">
+                  <span>{timer.hr}</span>
+                  <span className="text-xs text-orange-500">Hrs</span>
+                </span>
+                <span className="flex flex-col p-2 mr-4">
+                  <span>{timer.min}</span><span className="text-xs text-orange-500">mins</span>
+                </span>
+                <span className="flex flex-col p-2 mr-4">
+                  <span>{timer.sec}</span>
+                  <span className="text-xs text-orange-500">Sec</span>
+                </span>
+                <a href="https://gum.co/lapse_app" target="_blank">
+                  <button className="outline-none px-4 py-2 bg-orange-500 m-2 rounded-lg hover:ring-2 ring-orange-900 text-xs sm:text-sm">
+                    Get It for
+                    <span className="line-through stroke-orange-100 text-orange-100 font-bold px-1">$35</span>
+                    <span className="text-gray-900">$25</span>
+                  </button>
+                </a>
+              </div>
+            </div>
+            <div className="p-2 flex items-center space-x-4">
+              <span className='text-orange-900'>Avalilable on</span>  <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12.861 4.55483C13.3623 3.95654 13.71 3.13997 13.71 2.31531C13.71 2.20212 13.7019 2.08893 13.6776 2C12.8772 2.03234 11.907 2.5336 11.3249 3.21273C10.8722 3.73017 10.4517 4.55483 10.4517 5.37948C10.4517 5.50884 10.4679 5.63012 10.4841 5.67054C10.5326 5.67863 10.6134 5.6948 10.6943 5.6948C11.4219 5.6948 12.3274 5.2097 12.861 4.55483ZM13.4351 5.86458C12.2223 5.86458 11.2441 6.6003 10.6134 6.6003C9.9424 6.6003 9.06923 5.91309 8.01819 5.91309C6.02122 5.91309 4 7.56241 4 10.667C4 12.6074 4.74381 14.6529 5.67357 15.9707C6.46589 17.0864 7.16119 18 8.15563 18C9.15008 18 9.58666 17.3451 10.8075 17.3451C12.0606 17.3451 12.3355 17.9838 13.4351 17.9838C14.5184 17.9838 15.238 16.9894 15.9171 16.0111C16.6852 14.8873 17.0005 13.7959 17.0167 13.7393C16.952 13.7231 14.8742 12.8742 14.8742 10.5053C14.8742 8.45174 16.5073 7.53007 16.5963 7.4573C15.5291 5.91309 13.8878 5.86458 13.4351 5.86458Z" fill="black" fill-opacity="0.9" />
+              </svg>
+              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0)">
+                  <path d="M6.6607 8.98214V14.7946L0.571411 13.9554V8.98214H6.6607ZM6.6607 2.34821V8.23214H0.571411V3.1875L6.6607 2.34821ZM15.4286 8.98214V16L7.33034 14.8839V8.98214H15.4286ZM15.4286 1.14286V8.23214H7.33034V2.25893L15.4286 1.14286Z" fill="black" />
+                </g>
+                <defs>
+                  <clipPath id="clip0">
+                    <rect width="16" height="16" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+            </div>
+          </div>}
+        </div>
+      )
+    default:
+      return null
+  }
+}
+
 
 function Home({ posts }) {
   return (
@@ -43,47 +181,8 @@ function Home({ posts }) {
               <span className="text-3xl font-semibold font-mono">Lapse</span>
             </div>
             <h1 className="text-5xl font-bold p-4 pt-15 ">Record your screen in <span className='bg-gradient-to-r from-orange-500 to-orange-900 bg-clip-text text-transparent'>time-lapse</span></h1>
-            <div className="mt-5 inline-block rounded-xl bg-skin-secondary align-middle justify-center bg-orange-200 px-2">
-              <div className="flex">
-                <span className="flex flex-col p-2 mr-4">
-                  <span>2</span>
-                  <span className="text-xs text-orange-500">Days</span>
-                </span>
-                <span className="flex flex-col p-2 mr-4">
-                  <span>0</span>
-                  <span className="text-xs text-orange-500">Hrs</span>
-                </span>
-                <span className="flex flex-col p-2 mr-4">
-                  <span>49</span><span className="text-xs text-orange-500">mins</span>
-                </span>
-                <span className="flex flex-col p-2 mr-4">
-                  <span>32</span>
-                  <span className="text-xs text-orange-500">Sec</span>
-                </span>
-                <a href="https://gum.co/lapse_app" target="_blank">
-                  <button className="px-4 py-2 bg-orange-500 m-2 rounded-lg hover:ring-2 ring-orange-900 text-xs sm:text-sm">
-                    Get It for
-                    <span className="line-through stroke-orange-100 text-orange-100 font-bold px-1">$35</span>
-                    <span className="text-gray-900">$25</span>
-                  </button>
-                </a>
-              </div>
-            </div>
-            <div className="p-2 flex items-center space-x-4">
-              <span className='text-orange-900'>Avalilable on</span>  <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12.861 4.55483C13.3623 3.95654 13.71 3.13997 13.71 2.31531C13.71 2.20212 13.7019 2.08893 13.6776 2C12.8772 2.03234 11.907 2.5336 11.3249 3.21273C10.8722 3.73017 10.4517 4.55483 10.4517 5.37948C10.4517 5.50884 10.4679 5.63012 10.4841 5.67054C10.5326 5.67863 10.6134 5.6948 10.6943 5.6948C11.4219 5.6948 12.3274 5.2097 12.861 4.55483ZM13.4351 5.86458C12.2223 5.86458 11.2441 6.6003 10.6134 6.6003C9.9424 6.6003 9.06923 5.91309 8.01819 5.91309C6.02122 5.91309 4 7.56241 4 10.667C4 12.6074 4.74381 14.6529 5.67357 15.9707C6.46589 17.0864 7.16119 18 8.15563 18C9.15008 18 9.58666 17.3451 10.8075 17.3451C12.0606 17.3451 12.3355 17.9838 13.4351 17.9838C14.5184 17.9838 15.238 16.9894 15.9171 16.0111C16.6852 14.8873 17.0005 13.7959 17.0167 13.7393C16.952 13.7231 14.8742 12.8742 14.8742 10.5053C14.8742 8.45174 16.5073 7.53007 16.5963 7.4573C15.5291 5.91309 13.8878 5.86458 13.4351 5.86458Z" fill="black" fill-opacity="0.9" />
-              </svg>
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#clip0)">
-                  <path d="M6.6607 8.98214V14.7946L0.571411 13.9554V8.98214H6.6607ZM6.6607 2.34821V8.23214H0.571411V3.1875L6.6607 2.34821ZM15.4286 8.98214V16L7.33034 14.8839V8.98214H15.4286ZM15.4286 1.14286V8.23214H7.33034V2.25893L15.4286 1.14286Z" fill="black" />
-                </g>
-                <defs>
-                  <clipPath id="clip0">
-                    <rect width="16" height="16" fill="white" />
-                  </clipPath>
-                </defs>
-              </svg>
-            </div>
+           
+            <TimerComp type={"horizontal"}/>
           </div>
           <div className='relative'>
             <video className='rounded-md shadow-md top-24 sm:absolute' src="videos/lapse.webm" controls />
