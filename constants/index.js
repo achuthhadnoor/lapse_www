dcac0993539e8fe6ea3a7480f4604ae49bb5c578
achuthhadnoor <armorachyuth123@gmail.com>
2023-03-13 14:00:36 +0530

    [initial] remove old code

--- constants/index.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDUyPBh322dxSkM-H1ISdyLRgHaY5I_edM",
-    authDomain: "lapse-f0e72.firebaseapp.com",
-    databaseURL: "https://lapse-f0e72-default-rtdb.firebaseio.com",
-    projectId: "lapse-f0e72",
-    storageBucket: "lapse-f0e72.appspot.com",
-    messagingSenderId: "731156869877",
-    appId: "1:731156869877:web:ea1999bc292a2d4f014bbc"
-  };
