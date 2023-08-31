d5e1bc9fc5fb75f649af8bf4aada13990a06bbcf
achuthhadnoor <armorachyuth123@gmail.com>
2023-09-01 01:41:07 +0530

    timer and version update

--- components/timer.tsx
+++ components/timer.tsx
@@ -5,22 +5,23 @@ export default function Timer({ type }: { type: string }) {
   const [timer, setTimer] = useState({ days: 0, hr: 0, min: 0, sec: 0 })
   const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
-    const countDownDate = new Date('August 31, 2023 15:37:25').getTime()
+    const countDownDate = new Date('September 30, 2023 15:37:25').getTime()
     // Update the count down every 1 second
-    var x = setInterval(function () {
+    let x = setInterval(function () {
       // Get today's date and time
-      var now = new Date().getTime()
-
+      let now = new Date().getTime()
+      // const before = '25$';
+      // const after = '15$'
       // Find the distance between now and the count down date
-      var distance = countDownDate - now
+      let distance = countDownDate - now
 
       // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24))
-      var hours = Math.floor(
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24))
+      let hours = Math.floor(
         (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
       )
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000)
       setTimer({ days: days, hr: hours, min: minutes, sec: seconds })
       setIsLoading(false)
       // If the count down is finished, write some text
